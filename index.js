0c7c36211dee10e03f2ef0563b0a4db39984a878
himanshu <khatri.hk18@gmail.com>
2025-09-06 23:18:32 +0530

    Add initial bot setup and message reply

---
+++ index.js
@@ -0,0 +1,19 @@
+const {Client, GatewayIntentBits} = require("discord.js");
+require('dotenv').config()
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on('messageCreate', (msg) => {
+    console.log(msg.content)
+
+    if(msg.author.bot) return;
+    msg.reply('Hello, I am a bot!')
+})
+
+client.login(process.env.DISCORD_TOKEN)
